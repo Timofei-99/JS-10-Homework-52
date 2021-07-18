d7336c7d6af288c644ed8a12f7e06a6c9c1f0b31
Shevchenko Timofei <tima9953@gmail.com>
2021-07-18 16:36:08 +0600

    Created class and functions for combination

--- src/PokerHand/PokerHand.js
+++ src/PokerHand/PokerHand.js
@@ -1,47 +1,43 @@
 class PokerHand {
-    getFlash = (array) => {
-        let count = 0;
-        for (let i = 0; i < array.length; i++){
-            for (let j = i + 1; j < array.length; j++){
-                if (array[i].suit !== array[j].suit){
-                    count++;
-                };
-            };
-        };
-        return count;
-    };
-
-    getPare = (array) => {
+    Flash = (array) => {
         let count = 0;
         for (let i = 0; i < array.length; i++) {
             for (let j = i + 1; j < array.length; j++) {
-                if (array[i].rank === array[j].rank){
-                    count ++;
-                };
+                if (array[i].suit !== array[j].suit) {
+                    count++;
+                }
             };
         };
         return count;
     };
 
-
-    getCombination(array) {
-        if (this.getFlash(array) === 5){
-            return 'Флеш !!!'
+    Pare = (array) => {
+        let count = 0;
+        for (let i = 0; i < array.length; i++) {
+            for (let j = i + 1; j < array.length; j++) {
+                if (array[i].rank === array[j].rank) {
+                    count ++;
+                }
+            }
         };
+        return count;
+    };
 
-        if(this.getPare(array) === 1) {
+
+    myCombination(array) {
+        if (this.Flash(array) === 5) {
+            return 'Флеш!'
+        }
+
+        if(this.Pare(array) === 1) {
             return 'Одна Пара!'
-        } else if (this.getPare(array) === 2) {
+        } else if (this.Pare(array) === 2) {
             return 'Две Пары!'
-        } else if (this.getPare(array) === 3) {
-            return 'Ураа СЕТ!'
-        }else if(this.getPare(array)===4){
-            return 'Карэ!'
-        }else if(this.getPare(array)===0){
-            return 'Попробуй снова!'
+        } else if (this.Pare(array) === 3) {
+            return 'Тройка!'
         }
     };
-};
+}
 
 
 export default PokerHand;
